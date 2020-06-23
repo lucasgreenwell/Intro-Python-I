ab7c977014c4175de895e58079ce05a1cc859909
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-06-22 21:54:53 -0600

    finishes file_io file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -6,13 +6,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
 # Open up the "foo.txt" file (which already exists) for reading
+# this saves it as a weird data type, a textEncoding
+foo_text_file = open("foo.txt", 'r')
 # Print all the contents of the file, then close the file
+print(foo_text_file.read())
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
-# writing. Write three lines of arbitrary content to that file,
+# writing.
+bar_text_file = open("bar.txt", "w+")
+# Write three lines of arbitrary content to that file,
+bar_text_file.write("/n /n /n look i made a text file")
+bar_text_file.close()
+bar_text_file = open("bar.txt", "r")
+print(bar_text_file.read())
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
